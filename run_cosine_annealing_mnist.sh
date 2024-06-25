3d47b14020b62b5c8db8f41cd6989c7ba08e2712
Micha Henheik <finn.micha.henheik@stud.uni-hannover.de>
2024-06-25 23:41:28 +0200

    Increase compute time in runscripts

--- run_cosine_annealing_mnist.sh
+++ run_cosine_annealing_mnist.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=32G
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 #SBATCH --mail-user=finn.micha.henheik@stud.uni-hannover.de
 #SBATCH --mail-type=FAIL
 #SBATCH --output cosine_annealing_benchmark.out
