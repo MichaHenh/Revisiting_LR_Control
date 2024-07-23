fix: changed Softmax to LogSoftmax for torch hub models

ba_parameter_free_dac/dacbench_custom/custom_sgd.py
@@ -219,12 +219,13 @@ class CustomSGDEnv(SGDEnv):
             ) = sgd_utils.random_instance(rng, self.datasets)
         elif self.torchub_model[0]:
             hub_model = torch.hub.load(
+                # local model loading for offline mode
                 torch.hub.get_dir() + '/' + self.torchub_model[0],
                 self.torchub_model[1],
                 pretrained=self.torchub_model[2],
                 source='local'
             )
-            self.model = torch.nn.Sequential(hub_model, torch.nn.Softmax(dim=0))
+            self.model = torch.nn.Sequential(hub_model, torch.nn.LogSoftmax(dim=1))
         else:
             # Load model from config file
             self.model = sgd_utils.create_model(