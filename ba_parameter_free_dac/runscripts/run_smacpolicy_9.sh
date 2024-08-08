f625444adf345362fcedf0cb1738cfcfb3249831
Micha Henheik <finn.micha.henheik@stud.uni-hannover.de>
2024-08-08 14:57:04 +0200

    update runscript time limits

--- ba_parameter_free_dac/runscripts/run_smacpolicy_9.sh
+++ ba_parameter_free_dac/runscripts/run_smacpolicy_9.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=66
 #SBATCH --mem=512G
-#SBATCH --time=20:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-user=finn.micha.henheik@stud.uni-hannover.de
 #SBATCH --mail-type=FAIL
 #SBATCH --output %x.out
