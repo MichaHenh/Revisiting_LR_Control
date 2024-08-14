c3c6bbb1f8bda45fff66b8dc9915924fd54d70eb
Micha Henheik <finn.micha.henheik@stud.uni-hannover.de>
2024-08-14 16:35:46 +0200

    fix and create runscripts for SMAC policy

---
+++ ba_parameter_free_dac/runscripts/smacpolicy_cifar10/run_smacpolicy_2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=SMAC_policy_cifar10_2
+#SBATCH --partition=ai
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=66
+#SBATCH --mem=512G
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=finn.micha.henheik@stud.uni-hannover.de
+#SBATCH --mail-type=FAIL
+#SBATCH --output %x.out
+#SBATCH --error %x.err
+
+module load Miniconda3
+
+conda activate PFDAC
+
+cd ..
+python smac_policy.py --config-name=create_smacpolicy_cifar10 seed=2 -m
+
