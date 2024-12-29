2a2f1feab5ed86829c0f6e595dabb6f37d59e892
Micha Henheik <finn.micha.henheik@stud.uni-hannover.de>
2024-12-29 11:53:05 +0100

    cawr tuning runscript update

--- ba_parameter_free_dac/runscripts/tune_cawr_cifar10/run_tune_cawr_1.sh
+++ ba_parameter_free_dac/runscripts/tune_cawr_cifar10/run_tune_cawr_1.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=66
 #SBATCH --mem=512G
-#SBATCH --time=48:00:00
+#SBATCH --time=120:00:00
 #SBATCH --mail-user=finn.micha.henheik@stud.uni-hannover.de
 #SBATCH --mail-type=FAIL
 #SBATCH --output %x.out
@@ -16,4 +16,4 @@ module load Miniforge3
 conda activate PFDAC
 
 cd ../..
-python tune_cawr.py --config-name=tune_cawr_cifar10 seed=1 -m
+python tune_cawr.py --config-name=tune_cawr_cifar10 seed=1
