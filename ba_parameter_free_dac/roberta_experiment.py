8e9a54f974374ec6cad20c0763ea576e619f1297
Micha Henheik <finn.micha.henheik@stud.uni-hannover.de>
2025-02-26 01:14:49 +0100

    asda

--- ba_parameter_free_dac/roberta_experiment.py
+++ ba_parameter_free_dac/roberta_experiment.py
@@ -190,7 +190,7 @@ def setup_trainer(model, tokenized_datasets, optimizer_cfg):
         output_dir="./results",
         max_steps=23000,
         per_device_train_batch_size=16,  # Effective batch size = 64 * 4 GPUs = 256
-        per_device_eval_batch_size=256,
+        per_device_eval_batch_size=384,
         eval_accumulation_steps=16,
         save_steps=1000,
         save_total_limit=1,  # Keep only the last checkpoint
