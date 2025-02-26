1f7b28560d5d7401e4c55251ff440fc0753624b3
Micha Henheik <finn.micha.henheik@stud.uni-hannover.de>
2025-02-26 20:03:11 +0100

    dad

--- ba_parameter_free_dac/roberta_experiment.py
+++ ba_parameter_free_dac/roberta_experiment.py
@@ -196,7 +196,7 @@ def setup_trainer(model, tokenized_datasets, optimizer_cfg):
     training_args = TrainingArguments(
         output_dir="./results",
         max_steps=23000,
-        per_device_train_batch_size=16,  # Effective batch size = 64 * 4 GPUs = 256
+        per_device_train_batch_size=64,  # Effective batch size = 64 * 4 GPUs = 256
         per_device_eval_batch_size=256,
         eval_accumulation_steps=64,
         save_steps=1000,
