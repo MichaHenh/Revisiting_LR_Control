be88885cd5bebb0f53175633948f97dbf58ebf0a
Micha Henheik <finn.micha.henheik@stud.uni-hannover.de>
2025-03-09 14:51:25 +0100

    fix

--- ba_parameter_free_dac/roberta_experiment.py
+++ ba_parameter_free_dac/roberta_experiment.py
@@ -18,10 +18,10 @@ def get_free_port():
 def main_wrapper(cfg):
     if cfg.get("manual_ddp", False):
         free_port = str(get_free_port())
-
+        os.chdir('.')
         os.environ["MASTER_ADDR"] = "127.0.0.1"
         os.environ["MASTER_PORT"] = free_port
-        
+
         print(f"Launching distributed run with {cfg.nproc} processes.")
         result = mp.spawn(main_worker, nprocs=cfg.nproc, args=(cfg,), join=True)
         print(result)
