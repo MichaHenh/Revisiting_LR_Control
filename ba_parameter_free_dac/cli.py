70cbf94b896e0aa6776800e03cc78cddf9bccfcf
Micha Henheik <finn.micha.henheik@stud.uni-hannover.de>
2025-03-31 17:16:34 +0200

    remove memray

--- ba_parameter_free_dac/cli.py
+++ ba_parameter_free_dac/cli.py
@@ -2,8 +2,6 @@
 from __future__ import annotations
 
 import sys
-
-import memray
 from codecarbon import track_emissions
 import hydra
 from ba_parameter_free_dac import run_experiment
@@ -12,18 +10,9 @@ from ba_parameter_free_dac import run_experiment
 @track_emissions(offline=True, country_iso_code="DEU")
 def main(cfg):
     """Console script for ba_parameter_free_dac."""
-    if 'use_memray' in cfg and cfg['use_memray']:
-        with memray.Tracker("memray.bin"):
-            print(cfg)
-            print(f"Configuration loaded: {cfg.name}")
-            return run_experiment(cfg)
-    else:
-        print(cfg)
-        print(f"Configuration loaded: {cfg.name}")
-        return run_experiment(cfg)
-
-    return 0
-
+    print(cfg)
+    print(f"Configuration loaded: {cfg.name}")
+    return run_experiment(cfg)
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
