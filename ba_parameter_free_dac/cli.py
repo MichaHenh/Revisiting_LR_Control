a0afe33c207610a06e82b7d65f7c1c534128f732
Micha Henheik <finn.micha.henheik@stud.uni-hannover.de>
2025-03-31 17:17:17 +0200

    remove codecarbon

--- ba_parameter_free_dac/cli.py
+++ ba_parameter_free_dac/cli.py
@@ -2,12 +2,10 @@
 from __future__ import annotations
 
 import sys
-from codecarbon import track_emissions
 import hydra
 from ba_parameter_free_dac import run_experiment
 
 @hydra.main(version_base=None, config_path="configs", config_name="dadaptation_libsvm")
-@track_emissions(offline=True, country_iso_code="DEU")
 def main(cfg):
     """Console script for ba_parameter_free_dac."""
     print(cfg)
